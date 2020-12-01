318390bf696a50c5caf728b28f5df96aee889cc8
kimtaegwan <ab154@naver.com>
2020-12-01 23:27:41 +0900

    Add react-virtualized

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,30 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem'
 import './TodoList.scss'
-const TodoList = ({todos,onRemove,onToggle}) => {
+
+const TodoList = ({todos, onRemove, onToggle}) => {
+    const rowRenderer = useCallback( // List컴포넌트 사용하기위에 함수 생성
+        ({index, key, style}) => {
+            const todo = todos[index];
+            return(
+                <TodoListItem todo={todo} key={key} onRemove={onRemove} onToggle={onToggle} style={style} />
+            );
+        },
+        [onRemove, onToggle, todos]
+    )
     return (
-        <div className="TodoList">
-            {todos.map(todo => (
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
-            ))}
-        </div>
+        <List
+            className="TodoList"
+            width={512} // 전체 크기
+            height={513} // 전체 높이
+            rowCount={todos.length} // 항목 개수
+            rowHeight={57} // 항목 높이
+            rowRenderer={rowRenderer} // 항목을 렌터링할 때 쓰는 함수
+            list={todos} // 배열
+            style={{outline: 'none'}} // List에 기본 적용되는 outline 스타일 제거
+        /> // List 컴포넌트를 사용할때는 해당 리스트의 전체 크기와 각 항목의 높이, 각 항목을 렌더리할때 사용 하는 함수, 그리고 배열을 props로 넣어 주어야 한다
     )
 }
 
-export default TodoList
+export default React.memo(TodoList)
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -3,19 +3,21 @@ import{MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline} from 'react-ic
 import cn from "classnames"
 import './TodoListItem.scss'
 
-const TodoListItem = ({todo,onRemove,onToggle}) => {
+const TodoListItem = ({todo,onRemove,onToggle, style}) => {
     const {id,text,checked} = todo;
     return (
-        <div className="TodoListItem">
-            <div className={cn("checkbox", {checked})} onClick={()=>onToggle(id)}>
-                {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-                <div className="text">{text}</div>
-            </div>
-            <div className="remove" onClick={()=>onRemove(id)}> {/* 현재 자신이 가진 id를 넣어서 삭제 함수를 호출 */}
-                <MdRemoveCircleOutline/>
+        <div className="TodoListItem-virtualized"  style={style}>
+            <div className="TodoListItem">
+                <div className={cn("checkbox", {checked})} onClick={()=>onToggle(id)}>
+                    {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+                    <div className="text">{text}</div>
+                </div>
+                <div className="remove" onClick={()=>onRemove(id)}> {/* 현재 자신이 가진 id를 넣어서 삭제 함수를 호출 */}
+                    <MdRemoveCircleOutline/>
+                </div>
             </div>
         </div>
     )
 }
 
-export default React.memo(TodoListItem); // 컴포넌트의 리렌더링 방지
+export default React.memo(TodoListItem, (prevProps, nextProps) => prevProps.todo === nextProps.todo,); // 컴포넌트의 리렌더링 방지
